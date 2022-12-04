8f737821500d88f03feec4741dbc5e1455058791
EC2 Default User <ec2-user@ip-172-31-11-181.ap-northeast-1.compute.internal>
2022-12-04 05:03:39 +0000

    [add]非同期通信修正完了

--- rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
@@ -4,11 +4,13 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
+    @books = Book.all
   end
   
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
+    @books = Book.all
   end
 end
